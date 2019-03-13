6b958987f8364dc7d9b3ad0a4662efcca57ea5ba
Egor Suvorov <egor.suvorov@gmail.com>
2019-03-13 15:38:32 +0300

    Refactor Field.winner

--- field.py
+++ field.py
@@ -10,12 +10,14 @@ class Field:
 
     def winner(self):
         for y in range(3):
-            if (self.field[y][0] == self.field[y][1] ==
-                    self.field[y][2] != self.EMPTY):
+            if (self.field[y][0] != self.EMPTY and
+                all(self.field[y][x] == self.field[y][x]
+                    for x in range(3))):
                 return self.field[y][0]
         for x in range(3):
-            if (self.field[0][x] == self.field[1][x] ==
-                    self.field[2][x] != self.EMPTY):
+            if (self.field[0][x] != self.EMPTY and
+                all(self.field[y][x] == self.field[y][x]
+                    for y in range(3))):
                 return self.field[0][x]
         if (self.field[0][0] == self.field[1][1] ==
                 self.field[2][2] != self.EMPTY):
