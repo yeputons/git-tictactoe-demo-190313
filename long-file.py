55a2b7bcc6361b5a56d54b1657c19771711d696d
Egor Suvorov <egor.suvorov@gmail.com>
2019-03-13 16:04:32 +0300

    Add long file with bugs

---
+++ long-file.py
@@ -0,0 +1,20 @@
+if a == 0:
+    print(0)
+if a == 1:
+    print(1)
+if a == 2:
+    print(1)
+if a == 3:
+    print(3)
+if a == 4:
+    print(4)
+if a == 5:
+    print(5)
+if a == 6:
+    print(6)
+if a == 7:
+    print(7)
+if a == 8:
+    print(7)
+if a == 9:
+    print(8)
